2b28a7b790354fdd9747b085abba2d3623626729
Maria-Latysheva <mlatysheva@gmail.com>
2021-10-02 17:56:08 +0200

    feat: add adaptive for 1024px

--- museum-adaptive/js/gallery.js
+++ museum-adaptive/js/gallery.js
@@ -1,5 +1,12 @@
 const pictureInnerContainer = document.querySelector('.picture-inner-container');
 
+const img = document.createElement('img');
+img.classList.add('gallery-img')
+img.src = `assets/img/galery/galery1.jpg`;
+img.alt = `galery1`;
+pictureInnerContainer.append(img);
+
+
 function imageCache(base, firstNum, lastNum) {
   this.cache = [];
   var img;
@@ -16,9 +23,9 @@ var myCache = new imageCache("assets/img/gallery", 1, 15);
 myCache = shaffle(myCache);
 
 for (var i = 1; i<=myCache.length; i++) {
-  const img = document.createElement('img');
-  img.classList.add('gallery-img')
-  img.src = myCache[i];
-  img.alt = 'gallery';
-  pictureInnerContainer.append(img);
+  const newImg = document.createElement('img');
+  newImg.classList.add('gallery-img')
+  newImg.src = myCache[i];
+  newImg.alt = 'gallery';
+  pictureInnerContainer.append(newImg);
 }
